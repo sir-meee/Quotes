4359f884c4d58fab7491d0159d3f42ba14c47e14
sir-meee <sammymbevi@gmail.com>
2018-10-21 20:25:49 +0300

    add author

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,8 @@
 export class Quote {
   constructor(public id:number, public name:string){}
+  public showAuthor:boolean;
+  constructor(public id:number, public name:string, public author:string){
+    this.showAuthor=false
+  }
+
 }
